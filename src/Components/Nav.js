b1979abc003e957849e0fd3b57dd9968fb999876
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-25 12:13:54 +0000

    s

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,18 +4,20 @@ import LLName from './Assets/littlelemonlogo.png'
 function Nav() {
   return (
    <>
+    <div className='mainNav'>
     <div className='navigationPicture'>
        <img src={LLName} height={200} width={200}/>
     </div>
     <div className='navigationList'>
         <ul>
-            <li>Home</li>
+            <li><a>Home</a></li>
             <li>About</li>
             <li>Menu</li>
             <li>Reservation</li>
             <li>Order Online</li>
             <li>Login</li>
         </ul>
+    </div>
     </div>
    </>
   )
