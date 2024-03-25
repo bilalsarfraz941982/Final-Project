124965a59162fde90295eb5d3dd7fd9310fdd2fe
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-25 13:53:22 +0000

    s

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,8 @@ function Header() {
       <div className='leftbox'>
         <h2>Little Lemon</h2><br></br>
         <h3>Chicago</h3><br></br>
-        <p>Welcome to Little Lemon,<br></br> where we offer delicious and fresh cuisine<br></br> inspired by the flavors of the Mediterranean.<br></br> Our menu features a range of tasty <br></br>options that are perfect for any occasion,<br></br> all served in a warm and inviting atmosphere.</p>
+        <p>Welcome to Little Lemon,<br></br> where we offer delicious and fresh cuisine<br></br> inspired by the flavors of the Mediterranean.<br></br> Our menu features a range of tasty <br></br>options that are perfect for any occasion,<br></br> all served in a warm and inviting atmosphere.</p><br></br>
+        <button style={{backgroundColor:"rgb(244, 206, 20)" , height:"50px" , width:"200px" , borderRadius:"20px", fontSize:"large"}}>Reserve a table</button>
       </div>
       <div className='rightbox'>
         <img src={HeaderImage} height={400} width={400}/>
