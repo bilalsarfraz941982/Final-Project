698df4f80404fdd625ce2459d71b7526c2fa06ee
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-25 13:24:53 +0000

    s

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import HeaderImage from './Assets/restauranfood.jpg'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='headermain'>
+      <div className='leftbox'>
+        <h2>Little Lemon</h2><br></br>
+        <h4>Chicago</h4><br></br>
+        <p>Welcome to Little Lemon,<br></br> where we offer delicious and fresh cuisine<br></br> inspired by the flavors of the Mediterranean.<br></br> Our menu features a range of tasty <br></br>options that are perfect for any occasion,<br></br> all served in a warm and inviting atmosphere.</p>
+      </div>
+      <div className='rightbox'>
+        <img src={HeaderImage} height={250} width={300}/>
+      </div>
+    </div>
   )
 }
 
