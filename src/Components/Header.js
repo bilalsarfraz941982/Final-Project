83af6f0a27bb26ec4291d2ff21d71c1e013a15a6
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-26 13:24:00 +0000

    s

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import HeaderImage from './Assets/restauranfood.jpg'
-import Headerbg from './Assets/headerbg.jpg'
+import Headerbg from './Assets/headerbg.jpg';
+import {Routes, Route , Link} from 'react-router-dom';
+import BookingForm from './BookingForm';
 
 function Header() {
   return (
@@ -9,8 +11,11 @@ function Header() {
         <h2>Little Lemon</h2><br></br>
         <h3>Chicago</h3><br></br>
         <p>Welcome to Little Lemon,<br></br> where we offer delicious and fresh cuisine<br></br> inspired by the flavors of the Mediterranean.<br></br> Our menu features a range of tasty <br></br>options that are perfect for any occasion,<br></br> all served in a warm and inviting atmosphere.</p><br></br>
-        <button style={{backgroundColor:"rgb(244, 206, 20)" , height:"50px" , width:"200px" , borderRadius:"20px", fontSize:"large"}}>Reserve a table</button>
+        <Link to="./BookingPage" className='bookingPage'><button style={{backgroundColor:"rgb(244, 206, 20)" , height:"50px" , width:"200px" , borderRadius:"20px", fontSize:"large"}}>Reserve a table</button></Link>
       </div>
+      <Routes>
+        <Route path='./BookingPage' element={<BookingForm/>}></Route>
+      </Routes>
       <div className='rightbox'>
         <img src={HeaderImage} height={400} width={350}/>
       </div>
