2e745620ab662739df47549b56862f3f488f40dc
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-26 11:05:43 +0000

    s

--- src/Components/HightLights.js
+++ src/Components/HightLights.js
@@ -2,7 +2,7 @@ import React from 'react';
 import dish1 from './Assets/greek salad.jpg';
 import dish2 from './Assets/bruchetta.jpg'
 import dish3 from './Assets/lemon dessert.jpg';
-import basket from './Assets/Basket.jpg'
+import basket from './Assets/Basket.svg'
 
 function HightLights() {
   return (
@@ -14,7 +14,7 @@ function HightLights() {
       <div className='masterswmenu'>
         <div className='dish1'>
           <div className='img1'>
-            <img src={dish1} height={"300px"} width={"300px"}/>
+            <img src={dish1} height={"300px"} width={"300px"} style={{borderRadius:"20px"}}/>
           </div>
           <div className='nameandprice1'>
             <h3>Greek Salad</h3>
@@ -25,31 +25,39 @@ function HightLights() {
           </div>
           <div className='delivery'>
             <h3>Order a delivery</h3>
-            <img src={basket} height={"30px"} width={"30px"}/>
+            <img src={basket} height={"30px"} width={"30px"} />
           </div>
         </div>
         <div className='dish2'>
         <div className='img1'>
-            <img src={dish2} height={"300px"} width={"300px"}/>
+            <img src={dish2} height={"300px"} width={"300px"} style={{borderRadius:"20px"}}/>
           </div>
-          <div className='nameandprice1'>
+          <div className='nameandprice2'>
             <h3>Brunchetta</h3>
             <h4>$5.99</h4>
           </div>
           <div className='dish1disc'>
-            <p></p>
+            <p>Our Bruschetta is made from  grilled bread<br></br> that has been smeared with garlic and<br></br> seasoned with salt and olive<br></br> oil.</p>
+          </div>
+          <div className='delivery'>
+            <h3>Order a delivery</h3>
+            <img src={basket} height={"30px"} width={"30px"}/>
           </div>
         </div>
         <div className='dish3'>
         <div className='img1'>
-            <img src={dish3} height={"300px"} width={"300px"}/>
+            <img src={dish3} height={"300px"} width={"300px"} style={{borderRadius:"20px"}}/>
           </div>
-          <div className='nameandprice1'>
-            <h3>Greek Salad</h3>
-            <h4>$12.99</h4>
+          <div className='nameandprice3'>
+            <h3>Lemon dessert</h3>
+            <h4>$5.99</h4>
           </div>
           <div className='dish1disc'>
-            <p></p>
+            <p>This comes straight from grandma’s recipe <br></br>book, every last ingredient has been<br></br> sourced and is as authentic as can<br></br> be imagined.s</p>
+          </div>
+          <div className='delivery'>
+            <h3>Order a delivery</h3>
+            <img src={basket} height={"30px"} width={"30px"}/>
           </div>
         </div>
       </div>
