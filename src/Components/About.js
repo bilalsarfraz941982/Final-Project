02fe2bfb750cd10137d487a947873a282d67e717
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-26 12:02:17 +0000

    a

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import aboutImg from  './Assets/chefs.png'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='aboutMain'>
+      <div className='aboutMessage'>
+        <h1 style={{color:"rgb(244, 206, 20)"}}>Little Lemon</h1><br></br>
+        <h3 style={{color:"white"}}>Chicago</h3><br></br>
+        <p style={{color:"white"}}>Little Lemon is a Mediterranean-inspired <br></br>cocktail and wine bar that takes <br></br>pride in offering a tantalizing <br></br>menu centered around fresh seafood, <br></br>succulent meats, flavorful charcuterie, <br></br>and locally sourced ingredients.</p>
+      </div>
+      <div className='aboutImg'>
+        <img src={aboutImg} height={"350px"} width={"350px"} />
+      </div>
+    </div>
   )
 }
 
