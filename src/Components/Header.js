d65c0296759dd6c10ebe7dfff9b3b443f338dff0
Bilal Sarfraz <bilalsarfraz1982@gmail.com>
2024-03-27 10:28:37 +0000

    z

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderImage from './Assets/restauranfood.jpg'
 import Headerbg from './Assets/headerbg.jpg';
+import {Link , Route , Routes} from 'react-router-dom';
 
 function Header() {
   return (
@@ -9,7 +10,7 @@ function Header() {
         <h2>Little Lemon</h2><br></br>
         <h3>Chicago</h3><br></br>
         <p>Welcome to Little Lemon,<br></br> where we offer delicious and fresh cuisine<br></br> inspired by the flavors of the Mediterranean.<br></br> Our menu features a range of tasty <br></br>options that are perfect for any occasion,<br></br> all served in a warm and inviting atmosphere.</p><br></br>
-        <button style={{backgroundColor:"rgb(244, 206, 20)" , height:"50px" , width:"200px" , borderRadius:"20px", fontSize:"large"}}>Reserve a table</button>
+        <Link to='/bookingpage' className='reservationButton'><button style={{backgroundColor:"rgb(244, 206, 20)" , height:"50px" , width:"200px" , borderRadius:"20px", fontSize:"large"}}>Reserve a table</button></Link>
       </div>
       <div className='rightbox'>
         <img src={HeaderImage} height={400} width={350}/>
